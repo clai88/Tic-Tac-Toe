9d2e10a6f66e92724d97da460b82dfb0d6879cb1
Chris <clai88@gmail.com>
2016-08-16 22:10:48 -0400

    fixed bug w/ diag

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -42,10 +42,8 @@ class TicTacToe
       print_board
       break if won?
     end
-    if i % 2 == 0 && won?
-      puts "#{@p2} you are the winner."
-    elsif won?
-      puts "#{@p1} you are the winner."
+    if won?
+      puts i%2 == 0 ? "#{@p2} wins" : "#{@p1} wins."
     else
       puts "Tie game!"
     end
@@ -59,6 +57,7 @@ class TicTacToe
       j = ""
       [i,i+1,i+2].each {|k| j+= @board[k]}
       return true if j == x || j == o
+      # diagonal
       j = ""
       [i,i+4,i+8].each {|k| j+= @board[k]} if i == 0
       return true if j == x || j == o
@@ -68,7 +67,9 @@ class TicTacToe
       j = ""
       [i,i+3,i+6].each {|k| j+= @board[k]}
       return true if j == x || j == o
-      [i,i+2,i+4].each {|k| j+= @board[k]} if i == 0
+      # diagonal
+      j = ""
+      [i,i+2,i+4].each {|k| j+= @board[k]} if i == 2
       return true if j == x || j == o
     end
     return false
